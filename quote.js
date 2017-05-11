58f6b3be70e196602cd1e6e02807d1b21f3d847a
theallisonbrett <theallisonbrett@gmail.com>
2017-05-11 15:46:24 -0700

    Change HTTP to HTTPS

--- quote.js
+++ quote.js
@@ -1,12 +1,12 @@
 function randomQuote() {
   $.ajax({
-      url: "http://api.forismatic.com/api/1.0/?",
+      url: "https://api.forismatic.com/api/1.0/?",
       dataType: "jsonp",
       data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
       success: function( response ) {
         $("#random_quote").html("<p id='random_quote' class='lead text-center'>" +
           response.quoteText + "<br/>&dash; " + response.quoteAuthor + " &dash;</p>");
-        $("#tweet").attr("href", "http://twitter.com/home/?status=" + response.quoteText +
+        $("#tweet").attr("href", "https://twitter.com/home/?status=" + response.quoteText +
           ' (' + response.quoteAuthor + ')');
       }
   });
